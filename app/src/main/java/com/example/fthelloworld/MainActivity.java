7fa085f8a4c70e54573e49d72391a434e9cdda50
zukauskait <101106944+zukauskait@users.noreply.github.com>
2022-03-07 20:11:12 +0000

    Pakeitimai 1.1

--- app/src/main/java/com/example/fthelloworld/MainActivity.java
+++ app/src/main/java/com/example/fthelloworld/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.fthelloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.Random;
 
 import android.os.Bundle;
 import android.widget.TextView;
@@ -19,7 +20,13 @@ public class MainActivity extends AppCompatActivity {
         final TextView greetingTextView = findViewById(R.id.tvMain);
         greetingTextView.setText("Opa!");
     }
-
+    public void setColour(View view)
+    {
+        final TextView greetingTextView = findViewById(R.id.tvMain);
+        Random spalvos_id = new Random();
+        int color = spalvos_id.nextInt();
+        greetingTextView.setTextColor(color);
+    }
 
 
 }
