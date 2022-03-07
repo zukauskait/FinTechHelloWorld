b99a6bbd9538f85632521bed6acf1e5d6887e4eb
zukauskait <101106944+zukauskait@users.noreply.github.com>
2022-03-07 19:58:39 +0000

    Pakeitimai 1.0

--- app/src/main/java/com/example/fthelloworld/MainActivity.java
+++ app/src/main/java/com/example/fthelloworld/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.fthelloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void btnMainClick(View view)
+    {
+        final TextView greetingTextView = findViewById(R.id.tvMain);
+        greetingTextView.setText("Opa!");
+    }
+
+
+
 }
